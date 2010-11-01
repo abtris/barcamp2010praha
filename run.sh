2c7b663f0b493262565d2086082cf6cdcfb5a24b
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2010-11-01 16:39:56 +0100

    Update run script

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-sudo ant -f ./etc/build.xml -Dws=~/Sites/barcamp2010  -Drep_url=~/Sites/barcamp2010 -Dmodulename=demoApp -Dtarget_path=/tmp/ -Dmajor_version=1.0
+sudo ant -f ./etc/build.xml -Dws=~/Sites/barcamp2010  -Drep_url=~/Sites/barcamp2010 -Dmodulename=demoApp -Dtarget_path=/tmp/rpm -Dmajor_version=1.0
